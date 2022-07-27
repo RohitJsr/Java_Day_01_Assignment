afec5ee5d5ff55feffa4844e453ec595fc6a6d11
RohitJsr <102182218+RohitJsr@users.noreply.github.com>
2022-07-27 22:53:02 +0530

    Add files via upload

---
+++ MyFirstProject.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.println("Name : Rohit Kumar\n" +
+                "Father’s Name : Upendra Prasad\n" +
+                "Mother‘s Name : Renu Devi\n" +
+                "Age : 24\n" +
+                "Gender : Male\n" +
+                "Address : Jamshedpur,jharkhand\n" +
+                "Mobile No. : 9122308670");
+    }
+}
